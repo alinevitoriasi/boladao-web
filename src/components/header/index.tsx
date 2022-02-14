ef9cb5ceaca4036528cb65ad69ca1a7ac61aa9c6
Aline Vitória <38819683+alinevitoriasi@users.noreply.github.com>
2022-02-14 09:37:42 -0300

    :sparkles: Ajustes de estilo em componentes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
 
 const Header: React.FC = () => {
   return (
     <div>
-        <nav>
-          <Link to="/">Inicio </Link> |{" "}
-          <Link to="/sobre">Sobre </Link> |{" "}
-          <Link to="/contato">Contato </Link>|{" "}
-        </nav>
+      <nav>
+        <Link className='link-button' to='/'>
+          Inicio
+        </Link>
+        <Link className='link-button' to='/sobre'>
+          Sobre
+        </Link>
+        <Link className='link-button' to='/contato'>
+          Contato
+        </Link>
+      </nav>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
