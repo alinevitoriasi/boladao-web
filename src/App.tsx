280c1ee0c41aad3f1a58e4954d596f4d3b37dad7
alinevitoriasi <alinevitoriabsi@gmail.com>
2022-02-18 22:26:18 -0300

    :sparkles: Adiciona tela de postagem

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Box, createTheme, ThemeProvider } from '@mui/material';
 
-import AppBar from './components/appBar';
+import AppBar from './components/AppBar';
 
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import NewPosts from './pages/NewPosts';
+import Posts from './pages/Posts';
 
 const App = () => {
   const appTheme = createTheme({
@@ -27,13 +29,17 @@ const App = () => {
     <BrowserRouter>
       <ThemeProvider theme={appTheme}>
         <AppBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/sobre' element={<About />} />
-          <Route path='/contato' element={<Contact />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
-        </Routes>
+        <Box sx={{ backgroundColor: '#f6f6f6', height: '100vh' }}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sobre' element={<About />} />
+            <Route path='/contato' element={<Contact />} />
+            <Route path='/novopost' element={<NewPosts />} />
+            <Route path='/posts' element={<Posts />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
+          </Routes>
+        </Box>
       </ThemeProvider>
     </BrowserRouter>
   );
