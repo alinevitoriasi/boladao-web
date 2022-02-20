2fcf1e47a50005391b60cfd4413b0df8d7dd85e3
alinevitoriasi <alinevitoriabsi@gmail.com>
2022-02-20 03:13:10 -0300

    ✨ Adiciona exemplo sobre hooks 💚

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
 
 import './style.css';
+import Button from '../../components/Button';
+
+const Clique = ({ text, numero, setNumero }: any) => {
+  return (
+    <>
+      <Button
+        text={text}
+        sx={{ m: 5 }}
+        onClick={() => setNumero((numero: any) => numero + 1)}
+      />
+      <h1>{numero}</h1>
+    </>
+  );
+};
 
 const About = () => {
-  return <Grid container></Grid>;
+  const [numeroA, setNumeroA] = useState(0);
+  const [numeroB, setNumeroB] = useState(0);
+
+  useEffect(() => {
+    document.title = `Cliques: ${numeroA}`;
+  }, [numeroB]);
+
+  return (
+    <Grid container>
+      <Clique text='BOTAO A' numero={numeroA} setNumero={setNumeroA} />
+      <Clique text='BOTAO B' numero={numeroB} setNumero={setNumeroB} />
+    </Grid>
+  );
 };
 
 export default About;
