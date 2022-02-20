a0fe56b539a5074cb25bb39588dc7fc471562439
alinevitoriasi <alinevitoriabsi@gmail.com>
2022-02-20 03:06:06 -0300

    ✨ Adiciona o componente tag

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import MuiChip from '@mui/material/Chip';
+
+import { ChipTypeMap, Stack } from '@mui/material';
+
+interface ITag {
+  label?: string;
+  color?: ChipTypeMap['props']['color'];
+  size?: ChipTypeMap['props']['size'];
+  variant?: ChipTypeMap['props']['variant'];
+  onDelete?: React.EventHandler<any>;
+}
+
+const Tag = ({ label, color, size, variant, onDelete }: ITag) => {
+  return (
+    <MuiChip
+      label={label}
+      color={color}
+      variant={variant}
+      size={size}
+      onDelete={onDelete}
+      sx={{
+        marginRight: 2,
+      }}
+    />
+  );
+};
+
+export default Tag;
--- src/pages/NewPosts/index.tsx
+++ src/pages/NewPosts/index.tsx
@@ -4,6 +4,7 @@ import { Grid } from '@mui/material';
 import { Box } from '@mui/system';
 import TextBox from '../../components/TextBox';
 import Button from '../../components/Button';
+import Tag from '../../components/Tag';
 
 const NewPosts = () => {
   return (
@@ -24,7 +25,49 @@ const NewPosts = () => {
           backgroundColor: '#DAE1E1',
         }}
       >
-        <TextBox variant='outlined' rows={5} sx={{ paddingBottom: 3 }} />
+        <Tag
+          label='Teste'
+          color='primary'
+          onDelete={() => console.log('teste')}
+        />
+        <Tag
+          label='Teste'
+          color='secondary'
+          onDelete={() => console.log('teste')}
+        />
+        <Tag
+          label='Teste'
+          color='success'
+          onDelete={() => console.log('teste')}
+        />
+        <Tag
+          label='Teste'
+          color='error'
+          onDelete={() => console.log('teste')}
+        />
+
+        <TextBox
+          placeholder='Digite seu texto aqui'
+          rows={5}
+          sx={{
+            backgroundColor: '#ffffff',
+            borderRadius: 1,
+            marginTop: 3,
+            marginBottom: 3,
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: '#ffffff',
+              },
+              '&:hover fieldset': {
+                borderColor: '#ffffff',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: '#ffffff',
+              },
+            },
+          }}
+        />
+
         <Button text='Enviar' color='secondary' />
       </Box>
     </Grid>
