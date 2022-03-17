b77e5a6a8b367704100e9967e0b04bb83f1413ac
alinevitoriasi <alinevitoriabsi@gmail.com>
2022-03-17 00:37:18 -0300

    ✨ Adiciona componente para página não encontrada

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,12 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <Grid container>
+      <h1>PARA O MUNDO, TÔ PERDIDO</h1>
+    </Grid>
+  );
+};
+
+export default NotFound;
