f13bbc0bb1ab4d5fe5d21fafd982be005d481281
alinevitoriasi <alinevitoriabsi@gmail.com>
2022-04-02 01:16:30 -0300

    ✨ Adiciona tratamento para lista vazia

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 import Card from '../../components/Card';
 import api from '../../services/api';
+import Button from '../../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 interface Post {
   _id: string;
   text: string;
 }
 const Posts = () => {
+  const navigate = useNavigate();
   const [posts, setPosts] = useState<Post[]>([]);
 
   async function getPosts() {
@@ -21,15 +24,40 @@ const Posts = () => {
   }, []);
 
   return (
-    <Grid container sx={{ padding: 5 }}>
-      {posts?.map((post: Post) => {
-        return (
-          <Grid key={post._id} item md={6} sm={12}>
-            <Card text={post.text} />
+    <>
+      {posts?.length < 1 ? (
+        <Grid
+          container
+          direction='column'
+          justifyContent='center'
+          alignItems='center'
+          sx={{ paddingTop: '25vh' }}
+        >
+          <Grid item>
+            <Typography variant='h4'>Ainda não temos posts.</Typography>
+            <Typography variant='body1' sx={{ padding: '40px' }}>
+              Seja o primeiro a fazer uma postagem
+            </Typography>
+            <Button
+              text='Criar um post'
+              onClick={() => {
+                navigate('/novopost');
+              }}
+            />
           </Grid>
-        );
-      })}
-    </Grid>
+        </Grid>
+      ) : (
+        <Grid container>
+          {posts?.map((post: Post) => {
+            return (
+              <Grid item key={post._id} md={6} sm={12}>
+                <Card text={post.text} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
+    </>
   );
 };
 
