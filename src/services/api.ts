dc815b1873f7e1bbede5f8dfcd3a15be191e1e13
alinevitoriasi <alinevitoriabsi@gmail.com>
2022-11-06 02:12:29 -0300

    Altera url base da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'http://localhost:5000',
 });
 
 api.interceptors.request.use(async (config: any) => {
