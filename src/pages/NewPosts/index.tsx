0b7db9fb57fb422807422a2c1108810fd95ad064
alinevitoriasi <alinevitoriabsi@gmail.com>
2023-08-27 13:35:38 -0300

    ✨Adiciona novos campos para o formulário

--- src/pages/NewPosts/index.tsx
+++ src/pages/NewPosts/index.tsx
@@ -4,22 +4,31 @@ import { Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import TextBox from '../../components/TextBox';
 import Button from '../../components/Button';
-import Tag from '../../components/Tag';
+// import Tag from '../../components/Tag';
 import { useForm } from 'react-hook-form';
 import api from '../../services/api';
+import Input from '../../components/Input';
+import SelectComponent from '../../components/Select';
+import Radio from '../../components/Radio';
 
 interface IPostForm {
+  tipo: Array<string>;
+  local: string;
   text: string;
 }
 
 const NewPosts = () => {
-  const { control, handleSubmit, reset } = useForm<IPostForm>();
+  const { control, watch, handleSubmit, reset } = useForm<IPostForm>();
   const [loading, setLoading] = useState(false);
+  const [sucessMessage, setSuccessMessage] = useState(false);
+  console.log(watch());
 
   const onSubmit = async (data: IPostForm) => {
+    console.log('onSubmit', data);
     setLoading(true);
     try {
       await api.post('/posts', data);
+      setSuccessMessage(true);
       reset();
     } catch (error) {
       console.log(error);
@@ -35,71 +44,149 @@ const NewPosts = () => {
       alignItems='center'
       style={{ height: 'inherit' }}
     >
-      <Box
-        sx={{
-          p: 15,
-          boxShadow: 3,
-          borderRadius: 3,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          backgroundColor: '#FFFFFF',
-          minWidth: '40%',
-        }}
-      >
-        <Typography
-          variant='h4'
-          className='title'
-          sx={{ fontWeight: 800, paddingBottom: 5 }}
+      {sucessMessage ? (
+        <Box
+          sx={{
+            p: 15,
+            // boxShadow: 3,
+            // borderRadius: 3,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            backgroundColor: '#FFFFFF',
+            minWidth: '40%',
+          }}
         >
-          Nova publicação
-        </Typography>
-        <div style={{ marginBottom: 15, marginTop: 15 }}>
-          <Tag
-            label='Teste'
-            color='primary'
-            onDelete={() => console.log('teste')}
-          />
-          <Tag
-            label='Teste'
-            color='secondary'
-            onDelete={() => console.log('teste')}
-          />
-          <Tag
-            label='Teste'
-            color='success'
-            onDelete={() => console.log('teste')}
-          />
-          <Tag
-            label='Teste'
-            color='error'
-            onDelete={() => console.log('teste')}
-          />
-        </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextBox
-            label=''
-            variant='outlined'
-            name='text'
-            control={control}
-            placeholder='Digite seu texto aqui'
-            rows={5}
-            fullWidth={true}
-            sx={{
-              borderRadius: 1,
-              marginTop: 3,
-              marginBottom: 3,
-              width: '100%',
-            }}
-          />
+          <Typography variant='h4' className='title' sx={{ fontWeight: 800 }}>
+            Publicação Compartilhada!
+          </Typography>
+          <Typography variant='body1' sx={{ py: 10, width: 800 }}>
+            Obrigado por compartilhar sua história. Suas palavras têm um impacto
+            significativo e contribuem para promover a conscientização e a
+            mudança no ambiente acadêmico. Lembre-se de que cada experiência
+            compartilhada fortalece nossa comunidade e nos ajuda a construir um
+            espaço mais inclusivo e empático para todos.
+          </Typography>
           <Button
             color='secondary'
             size='large'
-            text='Enviar'
+            text='ir para tela inicial'
             type='submit'
             loading={loading}
           />
-        </form>
-      </Box>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            p: 15,
+            // boxShadow: 3,
+            borderRadius: 3,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            backgroundColor: '#FFFFFF',
+            minWidth: '40%',
+          }}
+        >
+          <Typography variant='h4' className='title' sx={{ fontWeight: 800 }}>
+            Compartilhe sua história
+          </Typography>
+          <Typography variant='body1' sx={{ width: 800 }}>
+            Você está contribuindo para um ambiente acadêmico mais inclusivo e
+            empático. Suas palavras podem inspirar a mudança e fortalecer nossa
+            comunidade.
+          </Typography>
+          <Typography variant='body1' sx={{ pb: 5, width: 800 }}>
+            Lembre-se de ser respeitoso e evitar divulgar informações sensíveis.
+          </Typography>
+          <Typography variant='body1' sx={{ pb: 5, width: 800 }}>
+            Juntos, estamos construindo um espaço onde todas as vozes são
+            valorizadas.
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <SelectComponent
+              label='Tipo'
+              name='tipo'
+              options={[
+                'Racismo',
+                'Machismo',
+                'LGBTfobia',
+                'Xenofobia',
+                'Gordofobia',
+                'Capicitismo',
+                'Etarismo',
+                'Intolerância Religiosa',
+                'Outros',
+              ]}
+              sx={{
+                width: '100%',
+                marginBottom: 4,
+              }}
+              control={control}
+            />
+            <Input
+              // error={!!errors.email}
+              // helperText={errors.email?.message || ' '}
+              control={control}
+              name='local'
+              label='Local'
+              variant='outlined'
+              sx={{
+                width: '100%',
+                marginBottom: 4,
+              }}
+            />
+            <Input
+              // error={!!errors.email}
+              // helperText={errors.email?.message || ' '}
+              control={control}
+              name='data'
+              label='Data'
+              type='date'
+              variant='outlined'
+              sx={{
+                width: '100%',
+                marginBottom: 2,
+              }}
+              InputLabelProps={{ shrink: true }}
+            />
+            <div
+              style={{
+                width: '100%',
+                marginBottom: 4,
+                marginTop: 0,
+              }}
+            >
+              Deseja manter sua identidade anônima?
+              <Radio name='anomimato' control={control}>
+                Sim
+              </Radio>
+              <Radio name='anomimato' control={control}>
+                Não
+              </Radio>
+            </div>
+            <TextBox
+              label=''
+              variant='outlined'
+              name='text'
+              control={control}
+              placeholder='Digite aqui sua experiência de discriminação'
+              rows={5}
+              fullWidth={true}
+              sx={{
+                marginBottom: 2,
+                width: '100%',
+              }}
+            />
+
+            <Button
+              color='secondary'
+              size='large'
+              text='Enviar'
+              type='submit'
+              loading={loading}
+            />
+          </form>
+        </Box>
+      )}
     </Grid>
   );
 };
